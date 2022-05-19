f8f848ce90b804852f09bda7628ee99e77ba96dd
mcyncierto <>
2022-05-19 11:43:12 +0800

    added unit test for checkout and checkin

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -14,6 +14,30 @@ class Book extends Model
         'author_id',
     ];
 
+    public function checkout($user)
+    {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+        ]);
+    }
+
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if (is_null($reservation)) {
+            throw new \Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at' => now(),
+        ]);
+    }
+
     public function path()
     {
         return '/books/'.$this->id;
@@ -24,5 +48,10 @@ class Book extends Model
         $this->attributes['author_id'] = (Author::firstOrCreate([
             'name' => $author,
         ]))->id;
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
